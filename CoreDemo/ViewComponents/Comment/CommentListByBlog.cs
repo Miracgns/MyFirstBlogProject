7799752825a9c5ad015d25f8c8375b9ff2cf4f45
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-15 19:52:58 +0300

    CommentList

---
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(8);
+            return View(values);
+
+
+        }
+
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,34 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    Username="Miraç"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Efe"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Ahmet"
+                }
+
+            };
+            return View(commentvalues);
+        }
+    }
+}
