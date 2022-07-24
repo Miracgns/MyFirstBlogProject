5770c6778eaa5f2248a35bccbe208d3477e8f172
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-25 01:57:16 +0300

    BlogProjem55'-Logo Eklendi

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -21,6 +21,12 @@ namespace CoreDemo.Controllers
             var values = bm.GetBlogByID(id);
             return View(values);
         }
-        
+
+        public IActionResult BlogListByWriter()
+        {
+
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
     }
 }
