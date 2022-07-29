94248ad1ea9565b4e4da4f4bcbd9211a4de5952a
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-30 01:15:42 +0300

    BlogProjem-APİ-POSTMAN//120.DERS

---
+++ BlogApiDemo/DataAccesLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccesLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-RV4J3FE;database=CoreBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee>  Employees { get; set; }
+    }
+}
