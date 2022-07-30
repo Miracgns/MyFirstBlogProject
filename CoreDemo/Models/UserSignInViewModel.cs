1a1766ec92627fc5edbfadac07936142098bc2e5
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-31 00:27:33 +0300

    BlogProjem-127-/Identity/Ajax

---
+++ CoreDemo/Models/UserSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lüften kullanıcı adını giriniz")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Lüften şifrenizi giriniz")]
+        public string password { get; set; }
+
+    }
+}
